7e0f704d8f258e1cd214e0c2dde3328848113474
Ravinga Perera <37566894+ravinga16@users.noreply.github.com>
2023-01-05 14:59:51 +1100

    Add files via upload

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux";
+import { selectAllPost } from "./postsSlice";
+
+const PostsList = () => {
+  const posts = useSelector(selectAllPost);
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+export default PostsList;
